efe73a0bfc1281b6b1878e6f5b42076d634faf12
shenalachintha <156101606+shenalachintha@users.noreply.github.com>
2025-10-12 23:36:08 +0530

    new commit

--- restaurant-app/src/Components/Order/OrderForm.js
+++ restaurant-app/src/Components/Order/OrderForm.js
@@ -1,16 +1,71 @@
 import React from 'react'
 import Form from '../../layouts/Form'
 import { Grid } from '@mui/material'
+import Input from '../../controls/input'
+import Select from '../../controls/select'
+import { useForm } from '../../hooks/useForm'
 
+
+
+const generateOrderNumber=()=>Math.floor(100000 + Math.random() * 900000);
+  
+
+const getFreshModelObject=()=>({
+"orderMasterId":0,
+"orderNumber":generateOrderNumber(),
+"customerId":'',
+"pMethod":'',
+"deletedOrderItemIds":"",
+"grandTotal":[],
+})
+
+const pMethods=[
+  {id:'cash', title:'Cash'},
+  {id:'card', title:'Card'},
+  {id:'upi', title:'UPI'},
+]
 function OrderForm() {
+  
+  useForm(getFreshModelObject)
+  
+
   return (
     <Form>
         <Grid container>
-            <Grid item xs={12} sm={6}>
-                Name
+            <Grid item xs={6} >
+                <Input 
+                disabled
+                name="order number"
+                 label="Order Number" 
+               
+                  value={values.orderNumber}
+                 />
+                 <Select
+                  name='customer id'
+                  label='Customer'
+                    onChange={handleInputChange}
+                  options={[
+                    {id:'1', title:'Customer 1'},
+                    {id:'2', title:'Customer 2'},
+                    {id:'3', title:'Customer 3'},
+                  ]}
+                 />
+               
             </Grid>
-            <Grid item xs={12} sm={6}>
-                Address
+            <Grid item xs={6}>
+              <Select
+              name='pmethod'
+              label='Payment Method'
+              value={values.pMethod}
+              options={pMethods}
+              onChange={handleInputChange}
+              />
+             <Input
+             name='grand total'
+              label='Grand Total'
+              value={values.grandTotal}
+            
+              />
             </Grid>
         </Grid>
 
--- restaurant-app/src/controls/input.js
+++ restaurant-app/src/controls/input.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import { TextField } from '@mui/material'
+import { TextField } from '@mui/material';
 
-function Input(props) {
-  return (
-    <TextField {...props} />
-    
-  )
+export default function Input(props) {
+
+    const { name, label, value, variant, onChange, error = null, ...other } = props;
+    return (
+        <TextField
+            variant={variant || "outlined"}
+            label={label}
+            name={name}
+            value={value}
+            onChange={onChange}
+            {...other}
+            {...(error && { error: true, helperText: error })}
+        />
+    )
 }
-
-export default Input
