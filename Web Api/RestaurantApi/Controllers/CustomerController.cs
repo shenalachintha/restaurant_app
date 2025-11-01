9c2dca6fb63d5f63ae4ec45f86222ab66b7eb1af
shenalachintha <156101606+shenalachintha@users.noreply.github.com>
2025-11-01 10:49:56 +0530

    new commit

---
+++ Web Api/RestaurantApi/Controllers/CustomerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantApi.Models;
+
+namespace RestaurantApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly RestaurentDbContext _context;
+
+        public CustomerController(RestaurentDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Customer
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        // GET: api/Customer/5
+      
+    }
+}
---
+++ Web Api/RestaurantApi/Controllers/FoodItemController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantApi.Models;
+
+namespace RestaurantApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodItemController : ControllerBase
+    {
+        private readonly RestaurentDbContext _context;
+
+        public FoodItemController(RestaurentDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/FoodItem
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FoodItem>>> GetFoodItems()
+        {
+            return await _context.FoodItems.ToListAsync();
+        }
+    }
+
+      
+}
